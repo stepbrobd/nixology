7b91f3049d256c23a259aecaf43d754a5025b2cb
Yifei Sun <ysun@hey.com>
2024-01-27 13:27:02 -0500

    example 1

---
+++ example1/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.writeShellApplication {
+          name = "cheese";
+          runtimeInputs = [ pkgs.cowsay ];
+          text = "cowsay cheese";
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
